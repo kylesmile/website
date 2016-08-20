722e5c0bae349ba5d6d0bbc1a646d2ce5f656e58
Kyle Smith <kyle@smithsrus.com>
2016-08-20 09:11:31 -0400

    Add config.development.example.js

---
+++ config.development.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  debug: true,
+  dbHost: 'localhost',
+  dbPort: '27017',
+  dbName: 'website',
+  sessionSecret: '98760ea919f758dabf38112b43e9563afd9ccdcfc428a2b30a2e682923919211ed10020be1645e383a521881e5e131f2f3c55bb6497fa7a13d7be1889720d087',
+  defaultUserEmail: 'default@example.com',
+  defaultUserPassword: 'password'
+}
